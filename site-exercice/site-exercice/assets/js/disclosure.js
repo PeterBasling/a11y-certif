4cebd32076185d755c073312b69f160263274066
jerome <weejeb@gmail.com>
2025-11-21 14:13:15 +0100

    5.2 disclosure

--- site-exercice/site-exercice/assets/js/disclosure.js
+++ site-exercice/site-exercice/assets/js/disclosure.js
@@ -5,8 +5,10 @@ function initDisclosure() {
             const isExpanded = event.target.classList.contains('expanded');
             event.target.classList.toggle('expanded');
             target.classList.toggle('expanded');
-        })
-    })
+            event.target.setAttribute('aria-expanded', String(!isExpanded));
+            target.style.display = !isExpanded ? 'block' : 'none';
+        });
+    });
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
